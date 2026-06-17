6cc2caffdb3f742771758973211930ccd12d91e6
universcom <hamed.enayatzare@gmail.com>
2026-06-17 02:04:27 +0200

    Add some comments

--- MCP/Client/01-First-Structure/client.py
+++ MCP/Client/01-First-Structure/client.py
@@ -5,14 +5,18 @@ class MCPclient:
     async def connect(self) -> None: 
         """
         Connect to the MCP server.
+        This will initialize your connection to the server. 
+        The implementation will look different based on what you choose for your transport layer
         """
     async def get_available_tools(self) -> list[Any]:
         """
         Get a list of available tools from the MCP server.
+        This will use the client’s server connection to retrieve the tools that the server makes available
         """
     async def execute_tool(self, tool_name: str, *args: Any) -> Any:
         """
         Execute a tool on the MCP server with the given arguments.
+        This will use the client’s server connection to retrieve the tools that the server makes available
         """
     async def disconnect(self) -> None:
         """
