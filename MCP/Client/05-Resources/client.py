043cf39e681f4adfd16476fc9f87d0b271a62c41
universcom <hamed.enayatzare@gmail.com>
2026-06-19 13:25:58 +0200

    update requirements file

---
+++ MCP/Client/05-Resources/client.py
@@ -0,0 +1,41 @@
+from mcp import ClientSession
+from contextlib import AsyncExitStack
+from mcp.client.stdio import StdioServerParameters, stdio_client
+from typing import Any
+import logging
+
+
+class MCPClinet:
+    def __init__(self, name: str, command: str, server_args: list[str], env_vars: dict[str, str]=None) -> None:
+        self.name = name
+        self.command = command
+        self.server_args = server_args
+        self.env_vars = env_vars or {}
+        self._session: ClientSession | None = None
+        self._exit_stack = AsyncExitStack()
+        self._connected: bool = False
+    async def connect(self) -> None:
+        if self._connected:
+            raise RuntimeError(f"{self.name} is already connected.")
+        
+        server_parameters = StdioServerParameters(
+            command=self.command,
+            args=self.server_args,
+            env=self.env_vars if self.env_vars else None
+        )
+
+        # Connect to stdio server, starting subprocess
+        stdio_connection = await self._exit_stack.enter_async_context(stdio_client(server_parameters))
+        self.read, self.write = stdio_connection
+
+        # Start MCP client session
+        self._session = await self._exit_stack.enter_async_context(ClientSession(read_stream=self.read, write_stream=self.write))
+
+        # Initialize session
+        await self._session.initialize()
+        self._connected = True
+    async def disconnect(self) -> None:
+        if self._exit_stack:
+            await self._exit_stack.aclose()
+            self._connected = False
+            self._session = None
