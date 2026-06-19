4e40839afc24a88950f5cdafdee09e416f285119
universcom <hamed.enayatzare@gmail.com>
2026-06-19 13:47:52 +0200

    Implemeting functions of get resources and resource_templates

--- MCP/Client/05-Resources/client.py
+++ MCP/Client/05-Resources/client.py
@@ -1,7 +1,7 @@
 from mcp import ClientSession
 from contextlib import AsyncExitStack
 from mcp.client.stdio import StdioServerParameters, stdio_client
-from typing import Any
+from mcp.types import Resource , ResourceTemplate
 import logging
 
 
@@ -34,8 +34,44 @@ class MCPClinet:
         # Initialize session
         await self._session.initialize()
         self._connected = True
+    
     async def disconnect(self) -> None:
         if self._exit_stack:
             await self._exit_stack.aclose()
             self._connected = False
             self._session = None
+    
+    async def get_available_resources(self) -> list[Resource]:
+        """
+        Fetches the list of available resources from the server and returns them as a list of Resource objects. Each Resource object contains the following information:
+        - uri: The unique URI identifying the resource.
+        - name: The name of the resource.
+        - description: A brief description of the resource.
+        - mimeType: The MIME type of the resource's content (e.g. "text/plain", "application/json").
+        """
+        if not self._connected:
+            raise RuntimeError("Client not connected to a server")
+        
+        resources_result = await self._session.list_resources()
+        if resources_result.resources is None:
+            logging.warning("No resources available from the server.")
+            return []
+        return resources_result.resources
+    
+    async def get_available_resource_templates(self) -> list[ResourceTemplate]:
+        """
+        Fetches the list of available resource templates from the server and returns them as a list of ResourceTemplate objects. Each ResourceTemplate object contains the following information:
+        - name: The name of the resource template.
+        - description: A brief description of the resource template.
+        - uriTemplate: The URI template (RFC 6570) describing how to construct a resource URI, including the parameters the client must supply.
+        - mimeType: The MIME type of the resources produced by this template, if known.
+        - annotations: Any additional metadata or annotations associated with the resource template, which can provide further context about its functionality or usage.
+        """
+        if not self._connected:
+            raise RuntimeError("Client not connected to a server")
+
+        templates_result = await self._session.list_resource_templates()
+        if templates_result.resourceTemplates is None:
+            logging.warning("No resource templates available from the server.")
+            return []
+        return templates_result.resourceTemplates
